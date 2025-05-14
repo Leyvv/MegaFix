d567e140471b4bb29eaa046181a5228fcce42886
daniel1819x <21011201@itsoeh.edu.mx>
2025-05-13 22:53:15 -0600

    Agregando base2.html

--- dash/templates/Graficas/Articulos.py
+++ dash/templates/Graficas/Articulos.py
@@ -7,7 +7,7 @@ import mysql.connector
 conexion = mysql.connector.connect(
 host = "localhost",
 user = "root",
-password = "Candelaria24",
+password = "Flores1819",
 database = "truper_ventas"
 )
 
